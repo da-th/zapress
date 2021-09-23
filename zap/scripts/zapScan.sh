efd1b3cc6f35032db33dd1c88179a90afc35465d
DanielT <daniel.thuermann@neofonie.de>
2021-09-23 09:38:32 +0200

    made spider and active scan run as before;

--- zap/scripts/zapScan.sh
+++ zap/scripts/zapScan.sh
@@ -45,111 +45,86 @@ fi
 
 # switch on all passive scanner
 allPassiveScannerOn() {
-scanInfo="$(curl -s 'http://localhost:8080/JSON/pscan/action/enableAllScanners')"
+  scanInfo="$(curl -s 'http://localhost:8080/JSON/pscan/action/enableAllScanners')"
 
-passiveScannerOn=$(echo ${scanInfo} | cut -c12-$((${#scanInfo}-2)))
+  passiveScannerOn=$(echo ${scanInfo} | cut -c12-$((${#scanInfo}-2)))
 
-echo "#########################################"
-echo "All passive scanner switched on: ${passiveScannerOn}"
-echo "#########################################"
-echo ""
+  echo "#########################################"
+  echo "All passive scanner switched on: ${passiveScannerOn}"
+  echo "#########################################"
+  echo ""
 }
 
+# reusable scan part for different taypes of scanning
 scan() {
   attempt_counter=0
   statusResult=""
 
   while
-    if [ ${attempt_counter} -eq ${max_attempts} ]; then
+    if [ ${attempt_counter} -eq ${max_attempts} ];then
       echo ""
-      echo "Max attempts reached, scan exiting"
+      echo "Max attempts reached, exiting"
       echo "#########################################"
       echo ""
+
+      # stop all running scans if it wasn't come to an end but exited
+      curl -s http://localhost:8080/JSON/${scanType}/action/stopAllScans > /dev/null
+      curl -s http://localhost:8080/JSON/ajaxSpider/action/stop > /dev/null
+
       exit 1
     fi
 
-    if [ ${scanType} = "ajaxSpider" ]; then
-      statusResult="$(curl -s 'http://localhost:8080/JSON/'${scanType}'/view/status')"
-      echo "Current scan status ($((${attempt_counter}+1))): $(echo ${statusResult} | cut -c12-$((${#statusResult}-2)))"
-    else
-      statusResult="$(curl -s 'http://localhost:8080/JSON/'${scanType}'/view/status/?scanId='${currentScanId})"
-      echo "Current scan status ($((${attempt_counter}+1))): $(echo ${statusResult} | cut -c12-$((${#statusResult}-2)))%"
-    fi
+    statusResult="$(curl -s 'http://localhost:8080/JSON/'${scanType}'/view/status/?scanId='${currentScanId})"
+    echo "Current scan status ($((${attempt_counter}+1))): $(echo ${statusResult} | cut -c12-$((${#statusResult}-2)))%"
 
-    attempt_counter="$(($attempt_counter+1))"
+    attempt_counter=$(($attempt_counter+1))
     sleep ${sleep}
 
-    [ "$statusResult" != "{\"status\":\"100\"}" ] || [ "$statusResult" != "{\"status\":\"stopped\"}" ]
+    [ "$statusResult" != "{\"status\":\"100\"}" ]
   do true; done
 }
 
 # spider
 spiderScan() {
-scanType="spider"
-sleep="2"
-max_attempts=50
+  scanType="spider"
+  sleep="2"
+  max_attempts=50
 
-scanInfo="$(curl -s 'http://localhost:8080/JSON/spider/action/scan/?url='${protocol}'%3A%2F%2F'${host}'%2F&recurse=true&inScopeOnly=false&scanPolicyName=&method=&postData=&contextId=')"
+  # stop maybe still running privious scan, so there is no mess up
+  curl -s http://localhost:8080/JSON/spider/action/stopAllScans > /dev/null
 
-currentScanId=$(echo ${scanInfo} | cut -c10-$((${#scanInfo}-2)))
+  scanInfo="$(curl -s 'http://localhost:8080/JSON/spider/action/scan/?url='${protocol}'%3A%2F%2F'${host}'%2F&recurse=true&inScopeOnly=false&scanPolicyName=&method=&postData=&contextId=')"
 
-echo "#########################################"
-echo "Started spider scan (ID: ${currentScanId})... "
-echo ""
+  currentScanId=$(echo ${scanInfo} | cut -c10-$((${#scanInfo}-2)))
 
-scan
+  echo "#########################################"
+  echo "Started spider scan (ID: ${currentScanId})... "
+  echo ""
 
-scanResults="$(curl -s 'http://localhost:8080/JSON/spider/view/results/?scanId='${currentScanId})"
-echo ""
-echo "Scan results: ${scanResults}"
-echo ""
-echo "Spider scan finished"
-echo "#########################################"
-echo ""
-}
+  scan
 
-# ajax spider
-ajaxSpiderScan() {
-scanType="ajaxSpider"
-contextName=""
-sleep="5"
-max_attempts=200
-
-scanInfo="$(curl -s 'http://localhost:8080/JSON/ajaxSpider/action/scan/?url='${protocol}'%3A%2F%2F'${host}'%2F&inScope=false&contextName='${contextName}'&subtreeOnly=true')"
-
-currentScanId=$(echo ${scanInfo} | cut -c10-$((${#scanInfo}-2)))
-
-echo "#########################################"
-echo "Started ajax spider scan... "
-echo ""
-
-scan
-
-scanResults="$(curl -s 'http://localhost:8080/JSON/ajaxSpider/view/results/?start=0&count=100')"
-echo ""
-echo "Scan results: ${scanResults}"
-echo ""
-echo "Ajax spider scan finished"
-echo "#########################################"
-echo ""
+  scanResults="$(curl -s 'http://localhost:8080/JSON/spider/view/results/?scanId='${currentScanId})"
+  echo ""
+  echo "Scan results: ${scanResults}"
+  echo ""
+  echo "Spider scan finished"
+  echo "#########################################"
+  echo ""
 }
 
 # active scan
 activeScan() {
-scanType="ascan"
-sleep="5"
-max_attempts=200
+  scanType="ascan"
+  sleep="5"
+  max_attempts=20
 
-scanInfo="$(curl -s 'http://localhost:8080/JSON/ascan/action/scan/?url='${protocol}'%3A%2F%2F'${host}'%2F&recurse=true&inScopeOnly=false&scanPolicyName=&method=&postData=&contextId=')"
+  # stop maybe still running privious scan, so there is no mess up
+  curl -s http://localhost:8080/JSON/ascan/action/stopAllScans > /dev/null
 
-currentScanId=$(echo ${scanInfo} | cut -c10-$((${#scanInfo}-2)))
+  scanInfo="$(curl -s 'http://localhost:8080/JSON/ascan/action/scan/?url='${protocol}'%3A%2F%2F'${host}'%2F&recurse=true&inScopeOnly=false&scanPolicyName=&method=&postData=&contextId=')"
 
-if echo "$currentScanId" | grep -Eq "[a-zA-Z]"; then
-  echo "####################################################################"
-  echo "ERROR: No scan (via cypress or spider) ran before this active scan."
-  echo "####################################################################"
-  return 0
-else
+  currentScanId=$(echo ${scanInfo} | cut -c10-$((${#scanInfo}-2)))
+
   echo "Started active scan (ID: ${currentScanId})... "
   echo ""
 
@@ -161,20 +136,18 @@ else
   echo ""
   echo "Active scan finished"
   echo "#########################################"
-fi
 }
 
 #####################################
 # main
 #####################################
 
-while [ $# > 0  ]; do
+
   opt="$1"
   case "$opt" in
-    -pso) shift && allPassiveScannerOn;;
-    -sp) shift && allPassiveScannerOn && spiderScan;;
-    -asp) shift && allPassiveScannerOn && ajaxSpiderScan;;
-    -as) shift && activeScan;;
-    -h|*) usage ;;
+    -pso) allPassiveScannerOn;;
+    -sp) spiderScan;;
+    -asp) ajaxSpiderScan;;
+    -as) activeScan;;
+    -h|*) usage;;
   esac
-done
