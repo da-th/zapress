73d373186c5bf44c95a6117f5636f303872f7c39
Daniel Thuermann <daniel.thuermann@neofonie.de>
2022-03-16 21:06:15 +0100

    make over of old tests and api tests;

--- cypress/integration/test.api.spec.js
+++ cypress/integration/test.api.spec.js
@@ -1,19 +1,38 @@
+/* Here are the first API tests. one can find some hints about it in the
+README.md. */
+
 describe('OWASP juice shop - API', () => {
 
-    it.only('Login', () => {
-      cy.api({
-        method: 'POST',
-        url: '/rest/user/login',
-        body: {
-          email: 'mc.safesearch@juice-sh.op',
-          password: 'Mr. N00dles'
-        }
-      }).then(
-        (response) => {
-          expect(response.status).to.eq(200),
-          expect(response.body.authentication.umail).to.eq('mc.safesearch@juice-sh.op'),
-          cy.log(response.body.authentication.token).as('token'),
-          cy.log(response.duration).as('duration')
-        })
-    })
-  })
+  it('Login', () => {
+    cy.api({
+      method: 'POST',
+      url: '/rest/user/login',
+      body: {
+        email: 'mc.safesearch@juice-sh.op',
+        password: 'Mr. N00dles'
+      }
+    }).then(
+      (response) => {
+        expect(response.status).to.eq(200),
+        expect(response.body.authentication.umail).to.eq('mc.safesearch@juice-sh.op'),
+        cy.log(response.body.authentication.token)
+          .as('token'),
+        cy.log(response.duration)
+          .as('duration')
+      });
+  });
+
+  it('Get legal text', () => {
+    cy.api({
+      method: 'GET',
+      url: '/ftp/legal.md'
+    }).then(
+      (response) => {
+        expect(response.status).to.eq(200),
+        cy.log(response.body)
+          .as('legal'),
+        cy.log(response.duration)
+          .as('duration')
+      });
+    });
+  });
--- cypress/integration/test.spec.js
+++ cypress/integration/test.spec.js
@@ -1,90 +1,90 @@
+/* I have revised the test cases. Now they are almost independent of each other,
+except the first test is responsible for setting the cookies correctly for the
+following ones. */
+
 describe("OWASP juice shop", () => {
-    before(() => {
-        cy.clearCookies();
-        cy.clearLocalStorage();
+  before(() => {
+    cy.clearCookies();
+    cy.clearLocalStorage();
+  });
+  beforeEach(() => {
+    Cypress.Cookies.preserveOnce('cookieconsent_status',
+      'welcomebanner_status');
+      cy.visit("/");
+  });
+
+  it("should close the welcome banner and cookie notice", () => {
+    cy.get("button[aria-label='Close Welcome Banner']")
+      .click();
+    cy.get("a[aria-label='dismiss cookie message']")
+      .click();
+  });
+
+  it("should type in credentials, press the login button and be logged in",
+    () => {
+    cy.get("button[aria-label='Show the shopping cart']").should("not.exist");
+
+    cy.get("button#navbarAccount").click();
+    cy.get("button#navbarLoginButton").click();
+
+    cy.url().should("include", "login");
+
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("mc.safesearch@juice-sh.op");
+      cy.get("#password").type("Mr. N00dles");
+      cy.get("#rememberMe").click();
+
+      cy.get("#loginButton").click();
     });
 
-    it("should visit some subpages", () => {
-        cy.visit("/#/about");
-        cy.visit("/#/photo-wall");
-        cy.request("/ftp/legal.md");
-      //  cy.visit("/ftp/", { failOnStatusCode: false }); // TODO make it run again
+    cy.get("button[aria-label='Show the shopping cart']").should("exist");
+  });
+
+  it("should visit some subpages", () => {
+    cy.visit("/#/about");
+    cy.visit("/#/photo-wall");
+  });
+
+  it("should open profile and add username", () => {
+    cy.get("button#navbarAccount").click();
+    cy.get("button#navbarLoginButton").click();
+
+    cy.url().should("include", "login");
+
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("mc.safesearch@juice-sh.op");
+      cy.get("#password").type("Mr. N00dles");
+      cy.get("#rememberMe").click();
+
+      cy.get("#loginButton").click();
     });
 
-    it("should close the welcome banner and cookie notice", () => {
-        cy.visit("/");
-        cy.get("button[aria-label='Close Welcome Banner']").click();
-        cy.get("a[aria-label='dismiss cookie message']").click();
-    });
+    cy.get("button[aria-label='Show the shopping cart']").should("exist");
 
-    it("should navigate to login page", () => {
-        cy.get("button#navbarAccount").click();
-        cy.get("button#navbarLoginButton").click();
+    cy.visit("/profile");
 
-        cy.url().should("include", "login");
-    });
+    cy.get("#username").clear().type("someUsername");
+    cy.get("#submit").click();
 
-    it("should type in credentials, press the login button and be logged in", () => {
-        cy.get("button[aria-label='Show the shopping cart']").should("not.exist");
+    cy.visit("/rest/user/data-export", { failOnStatusCode: false })
+  });
 
-        cy.get("#login-form").within(() => {
-            cy.get("#email").type("mc.safesearch@juice-sh.op");
-            cy.get("#password").type("Mr. N00dles");
-            cy.get("#rememberMe").click();
+  it("should open and close the item detail", () => {
+    cy.get(".ribbon-card").first().click();
+    cy.get("mat-dialog-container").should("exist");
 
-            cy.get("#loginButton").click();
-        });
+    cy.get("button[aria-label='Close Dialog']").click();
+    cy.get("mat-dialog-container").should("not.exist");
+  });
 
-        cy.get("button[aria-label='Show the shopping cart']").should("exist");
-    });
+  it("should visit the score board", () => {
+    cy.visit("/#/score-board");
+  });
 
-    it("should open profile and add username", () => {
-        cy.get("button#navbarAccount").click();
-        cy.get("button#navbarLoginButton").click();
+  it("should enter XSS payload into the search field", () => {
+    const payload = '<iframe src="javascript:alert(`xss`)">';
 
-        cy.url().should("include", "login");
-
-        cy.get("#login-form").within(() => {
-          cy.get("#email").type("mc.safesearch@juice-sh.op");
-          cy.get("#password").type("Mr. N00dles");
-          cy.get("#rememberMe").click();
-
-          cy.get("#loginButton").click();
-        });
-
-        cy.get("button[aria-label='Show the shopping cart']").should("exist");
-
-        cy.visit("/profile");
-
-        cy.get("#username").clear().type("someUsername");
-        cy.get("#submit").click();
-
-        cy.visit("/rest/user/data-export", { failOnStatusCode: false })
-    });
-
-    it("should open and close the item detail", () => {
-        cy.visit("/");
-        cy.get("button[aria-label='Close Welcome Banner']").click();
-        cy.get("a[aria-label='dismiss cookie message']").click();
-
-        cy.get(".ribbon-card").first().click();
-        cy.get("mat-dialog-container").should("exist");
-
-        cy.get("button[aria-label='Close Dialog']").click();
-        cy.get("mat-dialog-container").should("not.exist");
-    });
-
-    it("should visit the score board", () => {
-        cy.visit("/#/score-board");
-    })
-
-    it("should enter XSS payload into the search field", () => {
-        cy.get("button[aria-label='Close Welcome Banner']").click();
-        cy.get("a[aria-label='dismiss cookie message']").click();
-
-        const payload = '<iframe src="javascript:alert(`xss`)">';
-
-        cy.get("#searchQuery").click();
-        cy.get("#mat-input-0").clear().type(payload) + '{enter}';
-    })
+    cy.get("#searchQuery").click();
+    cy.get("#mat-input-0").clear().type(payload) + '{enter}';
+  });
 });
